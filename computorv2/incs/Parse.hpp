c4881785b8a6454249fee689752a3c2abc402b09
sleepychloe <sleepychloe444@gmail.com>
2024-11-28 11:08:40 +0100

    20241128_11:08

--- computorv2/incs/Parse.hpp
+++ computorv2/incs/Parse.hpp
@@ -6,13 +6,13 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 10:08:49 by yhwang            #+#    #+#             */
-/*   Updated: 2024/11/28 10:13:47 by yhwang           ###   ########.fr       */
+/*   Updated: 2024/11/28 11:07:36 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../matrix/incs/Complex.hpp"
-#include "../../matrix/incs/Vector.hpp"
-#include "../../matrix/incs/Matrix.hpp"
+#include "../matrix/incs/Complex.hpp"
+#include "../matrix/incs/Vector.hpp"
+#include "../matrix/incs/Matrix.hpp"
 
 class	Parse
 {
