1728776b0c4b17b4fafb688dfaf39b00c716f413
sleepychloe <sleepychloe444@gmail.com>
2024-11-30 23:33:23 +0100

    20241130_23:33

--- computorv2/incs/Parse.hpp
+++ computorv2/incs/Parse.hpp
@@ -6,9 +6,12 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 10:08:49 by yhwang            #+#    #+#             */
-/*   Updated: 2024/11/30 19:56:34 by yhwang           ###   ########.fr       */
+/*   Updated: 2024/11/30 23:20:58 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PARSE_HPP
+# define PARSE_HPP
 
 #include "../matrix/incs/Complex.hpp"
 #include "../matrix/incs/Vector.hpp"
@@ -16,6 +19,15 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <variant>
+#include <iterator>
+
+using V = std::variant<float, Complex<float>, Matrix<float>, Matrix<Complex<float>>, Vector<float>, Vector<Complex<float>>>;
+
+# define TYPE_VARIABLE		0
+# define TYPE_FUNCTION		1
+# define TYPE_CALCULATE		2
 
 class	Parse
 {
@@ -28,12 +40,19 @@ public:
 	void				parse_start(std::string &str);
 
 private:
+	int				check_keyword(std::string str);
+
 	int				is_equation_form(std::string str);
 	int				check_float_point(std::string str);
 	int				check_number(std::string str);
 	void				remove_space(std::string &str);
 
+	// void				check_type(std::string &str);
 	int				check_str(std::string &str);
 
-	std::string 			_err_msg;
+	std::map<std::string, V>		_var;
+	std::map<std::string, V>		_func;
+	std::string 				_err_msg;
 };
+
+#endif
--- computorv2/srcs/Parse.cpp
+++ computorv2/srcs/Parse.cpp
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 10:08:56 by yhwang            #+#    #+#             */
-/*   Updated: 2024/11/30 16:59:24 by yhwang           ###   ########.fr       */
+/*   Updated: 2024/11/30 23:32:57 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,60 @@ Parse::~Parse()
 void	Parse::parse_start(std::string &str)
 {
 	check_str(str);
+}
+
+int	Parse::check_keyword(std::string str)
+{
+	if (str == "var" || str == "VAR"
+		|| str == "vriable" || str == "VARIABLE")
+	{
+		
+		//test
+		this->_var["a"] = 1.1f;
+		this->_var["b"] = Complex<float>(1, 1);
+		this->_var["c"] = Matrix<float>({{1.1, 2.2}, {3.3, 4.4}});
+		this->_var["d"] = Matrix<Complex<float>>({{Complex<float>(1, 1), Complex<float>(2, 2)},
+							{Complex<float>(3, 3), Complex<float>(4, 4)}});
+		this->_var["e"] = Vector<float>({1.1, 2.2, 3.3});
+		this->_var["f"] = Vector<Complex<float>>({Complex<float>(1, 1), Complex<float>(2, 2), Complex<float>(3, 3)});
+		
+		std::cout << "variable list" << std::endl;
+		for (std::map<std::string, V>::iterator it = this->_var.begin();
+			it != this->_var.end(); it++)
+		{
+			if (it != this->_var.begin())
+				std::cout << std::endl;
+			std::cout << it->first << " = ";
+			if (std::get_if<float>(&it->second))
+				std::cout << *std::get_if<float>(&it->second);
+			else if (std::get_if<Complex<float>>(&it->second))
+				std::cout << *std::get_if<Complex<float>>(&it->second);
+			else if (std::get_if<Matrix<float>>(&it->second))
+				std::cout << *std::get_if<Matrix<float>>(&it->second);
+			else if (std::get_if<Matrix<Complex<float>>>(&it->second))
+				std::cout << *std::get_if<Matrix<Complex<float>>>(&it->second);
+			else if (std::get_if<Vector<float>>(&it->second))
+				std::cout << *std::get_if<Vector<float>>(&it->second);
+			else if (std::get_if<Vector<Complex<float>>>(&it->second))
+				std::cout << *std::get_if<Vector<Complex<float>>>(&it->second);
+			else
+			{
+				this->_err_msg = "detected unknown type while printing variable list";
+				this->_err_msg += ": std::map<std::string, V>";
+				this->_err_msg += "(V = std::variant<float, Complex<float>, Matrix<float>, Matrix<Complex<float>>,";
+				this->_err_msg += "Vector<float>, Vector<Complex<float>>>)";
+				throw (this->_err_msg);
+			}
+		}
+		std::cout << std::endl;
+		return (1);
+	}
+	// else if (str == "func" || str == "FUNC"
+	// 	|| str == "function" || str == "function")
+	// {
+	// 	return (1);
+	// }
+	return (0);
 }
 
 int	Parse::is_equation_form(std::string str)
@@ -175,8 +229,12 @@ void	Parse::remove_space(std::string &str)
 
 int	Parse::check_str(std::string &str)
 {
+	if (check_keyword(str))
+		return (1);
 	if (!(is_equation_form(str) && check_number(str)))
 		return (0);
 	remove_space(str);
+
+	// check_type(str);
 	return (1);
 }
